3235ec58e7a5ddacc2ca96e7cf99504f946abac5
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-03-10 18:38:53 -0500

    added post to receive req.body, save new note to db and return new note

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Basic route that sends the user first to the AJAX Page
+// * GET `/notes` - Should return the `notes.html` file.
 app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
@@ -21,10 +21,25 @@ app.get("/notes", function(req, res){
 app.get("*", function(req,res){
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
-// Grab the notes list (this should be updated for every new note and deleted note.)
+//* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
   res.json(database);
 });
+
+// * POST `/api/notes` - Should receive a new note to save on the request body, 
+// add it to the `db.json` file, and then return the new note to the client.
+app.post("/api/notes",function (req,res) {
+    let newNote = req.body;
+    database.push(newNote)
+    res.json(newNote);
+
+})
+
+//* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
+//This means you'll need to find a way to give each note a unique `id` when it's saved. 
+//In order to delete a note, you'll need to read all notes from the `db.json` file, 
+//remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
 
 // Starts the server to begin listening
 // =============================================================
