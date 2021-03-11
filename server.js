72a1e9cfbce1679be2e613044ec4cb9940f68abe
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-03-10 22:35:22 -0500

    fixed writeFile

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 //sets initial port for listeners
 const PORT = process.env.PORT || 8000;
 const database = require("./db/db.json");
+const { dirname } = require("path");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,28 +26,56 @@ app.get("*", function(req,res){
 app.get("/api/notes", function (req, res) {
   res.json(database);
 });
+app.get("/api/notes/:id", function(req, res) {
+  let savedNotes = JSON.parse(fs.readFileSync(database, "utf8"));
+  res.json(savedNotes[Number(req.params.id)]);
+});
 
 // * POST `/api/notes` - Should receive a new note to save on the request body, 
 // add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes",function (req,res) {
+    //let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
     let newNote = req.body;
-    let lastNoteId = database.length
-    newNote.id = lastNoteId + 1;
+    let newNoteId = database.length
+    newNote.id = newNoteId
     database.push(newNote)
-    fs.writeFile(path.join(__dirname, "/db/db.json", JSON.stringify(database), function (err) {
+      fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
       if (err) {
-      return console.log(err);
+          return console.log(err);
       }
-      console.log("Your note was saved!");
-    database.push(newNote)
-    
+      console.log("Note saved to db.json. Content: ", newNote);
+  });
+  
+      res.json(newNote)
+       
 })
 
 //* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
 //This means you'll need to find a way to give each note a unique `id` when it's saved. 
 //In order to delete a note, you'll need to read all notes from the `db.json` file, 
 //remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete("/api/notes/:id", function (req, res) {
+  
+  // request to delete note by id.
+  for (let i = 0; i < database.length; i++) {
 
+      if (database[i].id === req.params.id) {
+          // Splice takes i position, and then deletes the 1 note.
+          database.splice(i, 1);
+          break;
+      }
+  }
+  // // Write the db.json file again.
+  // fs.writeFileSync("/db/db.json", JSON.stringify(database), function (err) {
+
+  //     if (err) {
+  //         return console.log(err);
+  //     } else {
+  //         console.log("Your note was deleted!");
+  //     }
+  // });
+  res.json(database);
+});
 
 // Starts the server to begin listening
 // =============================================================
