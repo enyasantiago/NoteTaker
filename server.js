b2a9c5e5c989dcf4b9f0f0c6496dd3977a55a872
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-03-11 00:21:09 -0500

    fixing routes

--- server.js
+++ server.js
@@ -18,36 +18,41 @@ app.use(express.static("public"));
 app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
-//* GET `*` - Should return the `index.html` file
-app.get("*", function(req,res){
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
-//* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-app.get("/api/notes", function (req, res) {
-  res.json(database);
+// //* GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res) {
+  return res.json(database);
 });
+// });
+// app.get("/api/notes", function(req, res) {
+//   let savedNotes = JSON.parse(fs.readFileSync(database, "utf8"));
+//   res.json(savedNotes[Number(req.params.id)]);
+// });
 app.get("/api/notes/:id", function(req, res) {
   let savedNotes = JSON.parse(fs.readFileSync(database, "utf8"));
   res.json(savedNotes[Number(req.params.id)]);
 });
 
+//* GET `*` - Should return the `index.html` file
+app.get("*", function(req,res){
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 // * POST `/api/notes` - Should receive a new note to save on the request body, 
 // add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes",function (req,res) {
-    //let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let newNote = req.body;
-    let newNoteId = database.length
-    newNote.id = newNoteId
-    database.push(newNote)
-      fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
-      if (err) {
-          return console.log(err);
-      }
-      console.log("Note saved to db.json. Content: ", newNote);
+  
+  let newNote = req.body;
+  let newNoteId = database.length
+  newNote.id = newNoteId
+  database.push(newNote)
+  fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Note saved to db.json. Content: ", newNote);
   });
   
-      res.json(newNote)
-       
+  res.json(newNote)
+  
 })
 
 //* DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
@@ -58,27 +63,28 @@ app.delete("/api/notes/:id", function (req, res) {
   
   // request to delete note by id.
   for (let i = 0; i < database.length; i++) {
-
-      if (database[i].id === req.params.id) {
-          // Splice takes i position, and then deletes the 1 note.
-          database.splice(i, 1);
-          break;
-      }
+    
+    if (database[i].id === req.params.id) {
+      // Splice takes i position, and then deletes the 1 note.
+      database.splice(i, 1);
+      break;
+    }
   }
   // // Write the db.json file again.
   // fs.writeFileSync("/db/db.json", JSON.stringify(database), function (err) {
-
-  //     if (err) {
-  //         return console.log(err);
-  //     } else {
-  //         console.log("Your note was deleted!");
-  //     }
-  // });
-  res.json(database);
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
-});
+    
+    //     if (err) {
+      //         return console.log(err);
+      //     } else {
+        //         console.log("Your note was deleted!");
+        //     }
+        // });
+        res.json(database);
+      });
+      
+      // Starts the server to begin listening
+      // =============================================================
+      app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+      });
+      
